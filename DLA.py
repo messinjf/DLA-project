b86c15941e4d78934b59dcb4d25ef93c7cf8e3e4
Justin Messinger <messinjf@miamioh.edu>
2018-03-06 18:00:15 -0500

    optimizations

--- DLA.py
+++ DLA.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 
 def random_direction(dimension):
     """
@@ -29,22 +30,27 @@ def random_valid_direction(point, matrix):
                     and abs(i)+abs(j) > 0       # don't allow stationary movement
                     and matrix[neighbor[0]][neighbor[1]] == 0): 
                 choices.append(np.array([i,j]))
-    # No neighbor found so return false
-    if(len(choices) == 0):
-        print("Warning: created particle was trapped and had no available movement.")
-        return np.zeros(2)
+                
+    # No neighbor which means the particle is trapped. With the current
+    # technique, this situation should never happen
+    assert(len(choices) != 0)
+    
     idx = np.random.randint(len(choices))
     return choices[idx]
 
-def is_inbound(point, matrix):
+def is_inbound(point, matrix, radius=None):
     """ Determines if the specified point is inbounds of the
-    given matrix. """
+    given matrix. If max_radius is not None, then it will also
+    check if the point is inside the radius."""
     x = int(point[0])
     y = int(point[1])
     return (x >= 1
             and x < matrix.shape[1]-1
             and y >= 1
-            and y < matrix.shape[0]-1)
+            and y < matrix.shape[0]-1
+            and ((radius) == None
+                 or (get_radius_of_point(point, matrix) <= radius))
+            )
 
 
 def has_neighbor(point, matrix):
@@ -64,6 +70,22 @@ def has_neighbor(point, matrix):
     # No neighbor found so return false
     return False
 
+
+def get_radius_of_point(point, matrix):
+    """ Return the distance of the point from the center of the matrix. """
+    center = np.array([matrix.shape[0]/2, matrix.shape[1]/2])
+    return np.linalg.norm(np.array(point) - center)
+
+def get_max_radius_of_structure(matrix):
+    max_r = 0
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            # If there is a particle at the point
+            if(matrix[i][j] == 1):
+                r = get_radius_of_point([i, j], matrix)
+                if r > max_r:
+                    max_r = r
+    return max_r
   
 def random_normal_vector():
     """ returns a random 2D unit vector. """
@@ -76,7 +98,7 @@ def random_normal_vector():
     R[1,1] = np.cos(theta)
     return np.dot(R, direction)
 
-def DLA(particles, N=40, M=16, sticking_probablity=1.0):
+def DLA(particles, N=100, sticking_probablity=1.0):
     matrix = np.zeros((N,N))
     center = np.array([N//2, N//2])
     
@@ -96,19 +118,42 @@ def DLA(particles, N=40, M=16, sticking_probablity=1.0):
     
     while(numberOfSuccessfulSticks < particles):
         
-        # Initialize a random walker at a position around a circle of diameter
-        # M. Note that this is different from the book where the boundary of a
+        # Determine the max radius of the structure. This information
+        # is used to speed up the simulation by adjusting the launching
+        # circle to be at 2 times the max radius, and to remove particles
+        # that are 3 times the max radius. See project 10 for details.
+        max_radius = get_max_radius_of_structure(matrix)
+        launching_radius = np.round(2.0 * max_radius)
+        despawn_radius = np.round(3.0 * max_radius)
+        
+        # We spawn a random walker at 2 times the max radius of the structure.
+        # This check is to make sure that particles can't spawn outside of the
+        # array
+        assert(2 * launching_radius < N)
+        # Initialize a random walker at a position around a circle of radius
+        # 2 times the max_radius.
+        # Note that this is different from the book where the boundary of a
         # square is chosen instead.
-        random_walker = np.round(center + (M / 2.0) * random_normal_vector())
+        random_walker = np.round(center + launching_radius * random_normal_vector())
         
-        # Deal with the case where the walker spawns on an already occupied
-        # space
-        while (matrix[int(random_walker[0]), int(random_walker[1])] == 1):
-            random_walker = np.round(center + (M / 2.0) * random_normal_vector())
+        # Should not spawn on a particle unless we have done something wrong.
+        assert(matrix[int(random_walker[0]), int(random_walker[1])] != 1)
         
-        while(is_inbound(random_walker, matrix)):
-            #random_walker += random_direction(2)
-            random_walker += random_valid_direction(random_walker, matrix)
+        while(is_inbound(random_walker, matrix, despawn_radius)):
+            
+            # Get a random direction for the walker to travel in.
+            random_direction = random_valid_direction(random_walker, matrix)
+            
+            # Another optimization. If the walker is more than 4 spaces
+            # away from the structure, then we can increase the step size
+            # of the random walk. See project 11 for details.
+            current_radius = get_radius_of_point(random_walker, matrix)
+            if current_radius > max_radius + 4:
+                step_size = int(np.round(current_radius - max_radius - 2))
+                assert(step_size >= 1)
+                random_direction *= step_size
+            
+            random_walker += random_direction
             
             
             if(has_neighbor(random_walker, matrix)):
@@ -127,7 +172,10 @@ def DLA(particles, N=40, M=16, sticking_probablity=1.0):
     return images
 
 if __name__ == "__main__":
+    t0 = time.time()
     images = DLA(100)
+    t1 = time.time()
     plt.imshow(images[-1])
     plt.show()
+    print(t1-t0)
     
--- DLA_animation.py
+++ DLA_animation.py
@@ -4,19 +4,34 @@ from matplotlib import animation
 
 import DLA
 
-# Matplotlib needs to know where ffmpeg is located.
-plt.rcParams['animation.ffmpeg_path'] = 'C:\\ffmpeg\\bin'
 
-PARTICLES = 200
-N = 60
-M = 45
-STICKING_PROBABILITY = 0.1
 
-images = DLA.DLA(PARTICLES, N, M, STICKING_PROBABILITY)
+PARTICLES = 300
+N = 200
+STICKING_PROBABILITY = 1.0
+
+def remove_emptiness_in_image(images):
+    """ Our structure is small compared to the actual animation at the moment,
+    this is due to the fact that our spawning circle must also be inside the
+    matrix, which is now 2 times the max radius of the structure. What we could
+    do is clip each image based on the size of the final structure, so that the
+    structure better fits inside of the plot. """
+    final_image = images[-1]
+    max_radius = int(np.ceil(DLA.get_max_radius_of_structure(final_image)))
+    center_i = int(final_image.shape[0]//2)
+    center_j = int(final_image.shape[1]//2)
+    
+    for k in range(len(images)):
+        images[k] = images[k][center_i-max_radius:center_i+max_radius,center_j-max_radius:center_j+max_radius]
+    
+    return images
+
+images = DLA.DLA(PARTICLES, N, STICKING_PROBABILITY)
+images = remove_emptiness_in_image(images)
 
 # Initialize figure
 fig = plt.figure()
-plt.title("Particles = {}; N = {}; M = {}: prob = {}".format(PARTICLES, N, M, STICKING_PROBABILITY))
+plt.title("Particles = {}; N = {}; prob = {}".format(PARTICLES, N, STICKING_PROBABILITY))
 
 # Show the first image, and specify to allow for animations
 im = plt.imshow(images[0], animated=True)
@@ -27,10 +42,14 @@ def update(i):
     return im,
 
 movie = animation.FuncAnimation(fig, update, frames=len(images), repeat=True, interval=50, blit=True)
-plt.show()
+#plt.show()
 
 # Create the animation
 # NOTE: You must have FFMPEG installed and in your path to actually create the
 # animation
-#writer = animation.FFMpegWriter(fps=30, codec=None, bitrate=None, extra_args=None, metadata=None)
-#movie.save('basic_animation.mp4', writer=writer)
+# It's actually really painful to get ffmpeg to work properly. I found that
+# the best way to do it was to install it through anaconda with the following
+# command:
+#           conda install -c conda-forge ffmpeg
+writer = animation.FFMpegWriter(fps=30, codec=None, bitrate=None, extra_args=None, metadata=None)
+movie.save('prob_100.mp4', writer=writer)
